91bc5d001d755de304a8eb7628b4cbf77c98dc62
Claire <foxcoindev@gmail.com>
2020-08-22 16:06:07 +1200

    Added base servlet project

---
+++ src/main/java/createcamp/inclusivityreview/TestServlet.java
@@ -0,0 +1,34 @@
+package createcamp.inclusivityreview;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+public class TestServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = -5595471112661348949L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	{
+		//req.getParameter("myparam")
+		JSONObject obj = new JSONObject();
+		try(PrintWriter writer = resp.getWriter()) {
+			writer.write(obj.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		resp.setStatus(HttpServletResponse.SC_OK);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+	{
+		//req.getParameter("myparam")
+	}
+}
