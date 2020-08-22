b862439d1c7d063c688459c71024c79ed959e7d2
Claire <foxcoindev@gmail.com>
2020-08-23 09:24:37 +1200

    Added API with JS examples

---
+++ application/view/api.js
@@ -0,0 +1,22 @@
+reviewexample = {
+	username: "claire",
+	placeID: "",
+	safety: 4.0,
+	inclusivity: 4.0,
+	enjoyability: "Repudiandae minima earum rerum ratione molestiae maxime dolorem ut. Tempora quod est quaerat unde corporis nisi. Quidem quibusdam inventore laudantium impedit repellat. Vel libero voluptas accusamus quis dolore ipsum saepe."
+};
+
+placeexample = {
+	placeID: "ChIJ90utGdevOG0Rhmjc02ajcJo",
+	name: "Ivy Bar & Cabaret",
+	lat: -41.29098399999999,
+	lng: 174.7771863,
+}
+
+userexample = {
+	username: "claire",
+	fullname: "Claire Chambers",
+	tags = [
+		"trans", "female", "queer"
+	];
+};
