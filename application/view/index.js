df4cd1098c179372c21c8c2b7bf3927985c18ca7
Claire <foxcoindev@gmail.com>
2020-08-23 11:48:31 +1200

    Reviews now autoremove

--- application/view/index.js
+++ application/view/index.js
@@ -40,6 +40,9 @@ function onPlaceSelected(lonLat, place)
 	document.getElementById("leftinner").setAttribute("style", "");
 	document.getElementById("leftmore").setAttribute("style", "");
 	document.getElementById("placetitle").innerHTML = place.name;
+	reviews = document.getElementsByClassName("review");
+	while(reviews.length > 0)
+		document.getElementById("leftinner").removeChild(reviews[0]);
 	reviews = getReviewsByPlace(place.place_id);
 	for(let i = 0; i < reviews.length && i < 2; i++)
 		newReview(reviews[i]);
