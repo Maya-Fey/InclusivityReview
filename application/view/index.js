f34bf3f9f7191ad3957ad69d664dc6467a912f80
Claire <foxcoindev@gmail.com>
2020-08-23 13:10:07 +1200

    Added link to review page

--- application/view/index.js
+++ application/view/index.js
@@ -52,6 +52,7 @@ function onPlaceSelected(lonLat, place)
 	document.getElementById("placesafety").innerHTML = "Safety: " + formatScore(averages.safety);
 	document.getElementById("placeinclusivity").innerHTML = "Inclusivity: " + formatScore(averages.inclusivity);
 	document.getElementById("placeenjoyability").innerHTML = "Enjoyability: " + formatScore(averages.enjoyability);
+	document.getElementById("leftmore").setAttribute("href", "review.html?placeID=" + place.place_id);
 }
 
 function centerOnLocation()
